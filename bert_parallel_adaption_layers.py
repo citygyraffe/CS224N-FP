ecfd2d045f6ae2881030717e666011e30b78e5ea
Jai Sood <ankur.jai.sood@gmail.com>
2024-05-29 20:53:43 -0700

    Added PALs and Shared PALs

--- bert_parallel_adaption_layers.py
+++ bert_parallel_adaption_layers.py
@@ -2,17 +2,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from bert import BertLayer, BertModel
+from bert import BertLayer, BertModel, BertSelfAttention
 from utils import *
 
 DEBUG_OUTPUT = False
 NUM_TASKS_SUPPORTED = 3
 
 class BertLayerWithParallelAdaption(BertLayer):
+
+    # Shared across all layers NOT tasks
+    instance_counter = 0
+    shared_adaption_layers_first = []
+    shared_adaption_layers_second = []
+    shared_attn_layers = []
+
     def __init__(self, config, args):
         super(BertLayerWithParallelAdaption, self).__init__(config)
         
         self.device = torch.device('cuda') if args.use_gpu else torch.device('cpu')
+        self.adaption_mode = args.parallel_adaption_layers
 
         # This size was borrowed directly from Bert and PALS
         self.adaption_layer_size_pals = 204
@@ -20,39 +28,54 @@ class BertLayerWithParallelAdaption(BertLayer):
         
         # Set by the adaption layer type
         self.adaption_layer_size = None
-        self.Vd = None
-        self.Ve = None
 
         # These lists hold layers and parameters for each task
-        #self.Vd_list = []
-        #self.Ve_list = []
         self.adaption_layer_list_first = []
         self.adaption_layer_list_second = []
+        self.attn_list = []
+        pal_attn_config = copy.deepcopy(config)
 
-        if args.parallel_adaption_layers == 'low-rank':
+        if self.adaption_mode == 'low-rank':
             self.adaption_layer_size = self.adaption_layer_size_low_rank
 
             for _ in range(NUM_TASKS_SUPPORTED):
-                #self.adaption_layer_list.append(nn.Linear(self.adaption_layer_size, self.adaption_layer_size).to(self.device))
-                #self.Vd_list.append(nn.Parameter(torch.randn(self.adaption_layer_size, config.hidden_size, requires_grad=True)).to(self.device))
-                #self.Ve_list.append(nn.Parameter(torch.randn(config.hidden_size, self.adaption_layer_size, requires_grad=True)).to(self.device))
                 self.adaption_layer_list_first.append(nn.Linear(config.hidden_size, self.adaption_layer_size).to(self.device))
                 self.adaption_layer_list_second.append(nn.Linear(self.adaption_layer_size, config.hidden_size).to(self.device))
         
-        elif args.parallel_adaption_layers == 'pals':
+        elif self.adaption_mode == 'pals':
             self.adaption_layer_size = self.adaption_layer_size_pals
-            raise NotImplementedError('PALS adaption layer not yet implemented')
+            pal_attn_config.num_attention_heads = 6
+            pal_attn_config.hidden_size = self.adaption_layer_size
+
+            for _ in range(NUM_TASKS_SUPPORTED):
+                self.adaption_layer_list_first.append(nn.Linear(config.hidden_size, self.adaption_layer_size).to(self.device))
+                self.attn_list.append(BertSelfAttention(pal_attn_config).to(self.device))
+                self.adaption_layer_list_second.append(nn.Linear(self.adaption_layer_size, config.hidden_size).to(self.device))
+        
+        elif self.adaption_mode == 'pals-shared':
+            self.adaption_layer_size = self.adaption_layer_size_pals
+            pal_attn_config.num_attention_heads = 6
+            pal_attn_config.hidden_size = self.adaption_layer_size
+
+            if BertLayerWithParallelAdaption.instance_counter == 0:
+                print("Creating shared adaption layers")
+                for _ in range(NUM_TASKS_SUPPORTED):
+                    BertLayerWithParallelAdaption.shared_adaption_layers_first.append(nn.Linear(config.hidden_size, self.adaption_layer_size).to(self.device))
+                    BertLayerWithParallelAdaption.shared_adaption_layers_second.append(nn.Linear(self.adaption_layer_size, config.hidden_size).to(self.device))
+                    BertLayerWithParallelAdaption.shared_attn_layers.append(BertSelfAttention(pal_attn_config).to(self.device))
+            else:
+                print("Skipping creating shared adaption layers, already created!")
+                    
         else:
-            raise ValueError(f'Invalid adaption_layer_type: {args.parallel_adaption_layers}')
+            raise ValueError(f'Invalid adaption_layer_type: {self.adaption_mode}')
     
-    def forward_parallel_adaption(self, hidden_states, task):
+        # Count the number of instances of this class
+        BertLayerWithParallelAdaption.instance_counter += 1
+    
+    def forward_parallel_adaption(self, hidden_states, attention_mask, task):
         # 1. Adaption layer. TS(h) = Vd * g(Ve * h)
         # g is a low-rank linear transformation for 'low-rank' adaption layers
         # g is multi-head attention for 'pals' adaption layers
-
-        #adaption_layer = self.adaption_layer_list[task]
-        #self.Vd = self.Vd_list[task]
-        #self.Ve = self.Ve_list[task]
 
         batch_size, seq_length, hidden_size = hidden_states.size()
         flattened_hidden_states = hidden_states.view(-1, hidden_size)
@@ -64,16 +87,28 @@ class BertLayerWithParallelAdaption(BertLayer):
             print("SIZEOF Ve transpose", self.Ve.transpose(0, 1).size())
             print("SIZEOF Vd", self.Vd.size())
             print("SIZEOF flattened_hidden_states", flattened_hidden_states.size())
-
-        #intermediate_output = torch.matmul(flattened_hidden_states, self.Ve)
-        #intermediate_output = adaption_layer(intermediate_output)
-        #adaption_output = torch.matmul(intermediate_output, self.Vd)
             
-        adaption_output = self.adaption_layer_list_first[task](flattened_hidden_states)
-        adaption_output = F.gelu(adaption_output)
-        adaption_output = self.adaption_layer_list_second[task](adaption_output)
-        adaption_output = adaption_output.view(batch_size, seq_length, hidden_size)
+        adaption_output = None
+        if self.adaption_mode == 'low-rank':
+            adaption_output = self.adaption_layer_list_first[task](flattened_hidden_states)
+            adaption_output = F.gelu(adaption_output)
+            adaption_output = self.adaption_layer_list_second[task](adaption_output)
+        elif self.adaption_mode == 'pals':
+            adaption_output = self.adaption_layer_list_first[task](flattened_hidden_states)
+            adaption_output = adaption_output.view(batch_size, seq_length,  self.adaption_layer_size)
+            adaption_output = self.attn_list[task].forward(adaption_output, attention_mask)
+            adaption_output = self.adaption_layer_list_second[task](adaption_output)
+            adaption_output = F.gelu(adaption_output)
+        elif self.adaption_mode == 'pals-shared':
+            adaption_output = BertLayerWithParallelAdaption.shared_adaption_layers_first[task](flattened_hidden_states)
+            adaption_output = adaption_output.view(batch_size, seq_length,  self.adaption_layer_size)
+            adaption_output = BertLayerWithParallelAdaption.shared_attn_layers[task].forward(adaption_output, attention_mask)
+            adaption_output = BertLayerWithParallelAdaption.shared_adaption_layers_second[task](adaption_output)
+            adaption_output = F.gelu(adaption_output)
+        else:
+            raise ValueError(f'Invalid adaption_layer_type: {self.adaption_mode}')
 
+        adaption_output = adaption_output.view(batch_size, seq_length, hidden_size)
         if DEBUG_OUTPUT:
             print("SIZEOF adaption_output", adaption_output.size())
 
@@ -85,7 +120,7 @@ class BertLayerWithParallelAdaption(BertLayer):
         attn_output = self.self_attention(hidden_states, attention_mask)
 
         # 1a. Parallel adaption layer. TS(h)
-        adaption_output = self.forward_parallel_adaption(hidden_states, task)
+        adaption_output = self.forward_parallel_adaption(hidden_states, attention_mask, task)
 
         # 2. Add-norm for multi-head attention. LN(h + MH(h))
         attn_output_normalized = self.add_norm(hidden_states, attn_output + adaption_output, self.attention_dense, self.attention_dropout, self.attention_layer_norm)
